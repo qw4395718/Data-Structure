79bba097e996b2b5859719419d51c17b93ce7753
qw4395719 <1242703910@qq.com>
2021-04-15 13:11:12 +0800

    //ZYF:二叉树的三种遍历

--- DataStruct_Ch06Tree/DataStruct_Ch06Tree/Ch06test01.cpp
+++ DataStruct_Ch06Tree/DataStruct_Ch06Tree/Ch06test01.cpp
@@ -25,11 +25,12 @@ typedef struct
 	int level, order;
 }Position;
 
-//TElemType visit(SqBiTree T)
-//{
-//	for (int i = 0; T[i] != NULL;i++)
-//	printf("%d", T[i]);
-//}
+Status visit(TElemType c)
+{
+	printf("%d ", c);
+	return OK;
+}
+
 TElemType Nil = 0; /*  设整型以0为空 */
 
 Status InitBiTree(SqBiTree T)
@@ -101,8 +102,93 @@ Status Root(SqBiTree T, TElemType *e)
 /* 操作结果:返回处于位置e的结点的值 */
 Status Value(SqBiTree T, Position e)
 {
-
+	return T[(int)powl(2, e.level-1) + e.order - 1];
 }
+
+/* 初始条件:二叉树存在,e是T中的某个节点 */
+/* 操作结果:若e是T的非根节点,则返回他的双亲,否则返回"否" */
+TElemType Parent(SqBiTree T, TElemType e)
+{
+	int i;
+	if (T[0] == Nil)
+		return Nil;
+	for (i = 0; i <= MAX_TREE_SIZE - 1; i++)
+	{
+		if (T[i] == e)
+			return T[(i +1)/ 2 - 1];
+	}
+	return Nil;/* 未找到其双亲 */
+}
+
+/* 初始条件:二叉树T存在,e是T中的某个节点 */
+/* 操作结果:*/
+TElemType LeftChild(SqBiTree T, TElemType e)
+{
+	int i;
+	if (T[0] == Nil)
+	{
+		return Nil;//空树
+	}
+	for (i = 0; i < MAX_TREE_SIZE - 1; i++)
+	{
+		if (T[i] == e)
+			return T[i * 2];
+	}
+	return Nil;
+}
+
+TElemType RightChild(SqBiTree T, TElemType e)
+{
+	int i;
+	if (T[0] == Nil)
+	{
+		return Nil;//空树
+	}
+	for (i = 0; i < MAX_TREE_SIZE - 1; i++)
+	{
+		if (T[i] == e)
+			return T[i * 2+1];
+	}
+	return Nil;
+}
+
+/* 初始条件: 二叉树存在,e是其中的一个节点 */
+/* 操作结果: 返回e的左兄弟,若e是T的左孩子或者无左兄弟,则返回空*/
+TElemType LeftSibling(SqBiTree T, TElemType e)
+{
+	int i;
+	if (T[0] == Nil)
+		return Nil;
+	for (i = 1; i <= MAX_TREE_SIZE - 1;i++)
+	if (T[i] == e&&i % 2 == 0)
+		return T[i - 1];
+	return Nil;
+}
+
+/* 初始条件: 二叉树T存在,e是T中某个结点 */
+/* 操作结果: 返回e的右兄弟。若e是T的右孩子或无右兄弟,则返回＂空＂ */
+TElemType RightSibling(SqBiTree T, TElemType e)
+{
+	int i;
+	if (T[0] == Nil) /* 空树 */
+		return Nil;
+	for (i = 1; i <= MAX_TREE_SIZE - 1; i++)
+	if (T[i] == e&&i % 2) /* 找到e且其序号为奇数(是左孩子) */
+		return T[i + 1];
+	return Nil; /* 没找到e */
+}
+
+/* PreOrderTraverse()调用 */
+void PreTraverse(SqBiTree T, int e)
+{
+	visit(T[e]);
+	if (T[2 * e + 1] != Nil) /* 左子树不空 */
+		PreTraverse(T, 2 * e + 1);
+	if (T[2 * e + 2] != Nil) /* 右子树不空 */
+		PreTraverse(T, 2 * e + 2);
+}
+
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
